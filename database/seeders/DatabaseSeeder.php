4eece2bf41199d06a15a8860043a3eb54294a14f
Ernesto Orellana <jorellana@homeland.com.gt>
2022-05-06 23:45:36 -0600

    Resources Hechos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(PermissionsSeeder::class);
         Clientes::factory(100)->create();
     }
 }
